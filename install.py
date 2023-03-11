9bb0d69536e3a9aae5ceb39edb3d2532ac948096
ArpanGyawali <arpan.gyawali7@gmail.com>
2023-03-11 20:30:34 +0545

    slight change in script for automatically updating requirements.txt

--- install.py
+++ install.py
@@ -1,14 +1,6 @@
 import os
 import sys
 
-def update_requirements():
-    # Get the list of installed packages
-    output = os.popen('pip freeze').read()
-
-    # Write the list of packages to the requirements.txt file
-    with open('requirements.txt', 'w') as f:
-        f.write(output)
-
 if __name__ == '__main__':
     if sys.argv[1] == '-u':
         for package in sys.argv[2:]:
@@ -20,4 +12,4 @@ if __name__ == '__main__':
             os.system(f'pip install {package}')
 
     # Update the requirements.txt file
-    update_requirements()
+    os.system('pip freeze > requirements.txt')
